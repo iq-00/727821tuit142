4eaebddc85117ecadec4f5f47c25317d1b2aa0ec
iq-00 <subhasbharathi@gmail.com>
2024-06-14 13:43:06 +0530

    completing authentication and prime

--- functions.mjs
+++ functions.mjs
@@ -1,6 +1,33 @@
 import { log } from "console"
-import http from "http"
-
+import axios from "axios"
 export default class functions {
 
+    prime(token) {
+        return new Promise(async (res, rej) => {
+            let value = (await axios.get("http://20.244.56.144/test/primes", { headers: { "Authorization": "Bearer "+token } })).data
+            res(value)
+        })
+    }
+
+    refreshToken() {
+        let token = undefined
+        let obj = {
+            "companyName": "top_products",
+            "clientID": "d4118401-b991-453c-8980-dd23f8315ee7",
+            "clientSecret": "KSLflHqGmryTPNdE",
+            "ownerName": "Subhas",
+            "ownerEmail": "727821tuit142@skct.edu.in",
+            "rollNo": "727821tuit142"
+        }
+        return new Promise((res, rej) => {
+            async function a() {
+                return (await axios.post("http://20.244.56.144/test/auth", JSON.stringify(obj))).data
+            }
+            a()
+                .then(ev => {
+                    token = ev.access_token
+                    res(token)
+                })
+        })
+    }
 }
--- server.mjs
+++ server.mjs
@@ -1,20 +1,32 @@
 import http from "http"
 import functions from "./functions.mjs"
 
-let fun=new functions()
+let fun = new functions()
 
 let server = http.createServer()
 
-server.on("request", (request, response) => {
+let authToken = undefined
+
+
+server.on("request", async (request, response) => {
     console.log(request.url);
-    
+
+    if (!authToken) {
+        authToken = await fun.refreshToken()
+        console.log(authToken);
+    }
+
+    if (request.url.startsWith("/numbers/p")) {
+        console.log(authToken);
+        let val = await fun.prime(authToken)
+        console.log(val);
+    }
+
 })
 
 
 
 // server running on default port
-server.listen(80, ev => {
+server.listen(80, "192.168.247.121", ev => {
     console.log("server running on default post");
 })
-
-// , "192.168.247.121"
